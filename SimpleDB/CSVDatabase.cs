2845e79d060e5fc4caef2a871bb8049932311ab3
Emma <ekra@itu.dk>
2024-09-04 15:04:06 +0200

    Created IDatabase Interface
    This is directly from the lecture
    
    Co-authored-by: Helge <ropf@itu.dk>

---
+++ SimpleDB/CSVDatabase.cs
@@ -0,0 +1,16 @@
+CSVDatabase {
+
+    
+}
+
+
+
+
+/*
+IDatabaseRepository<Cheep> database = new CSVDatabase<Cheep>();
+...
+var cheep = new Cheep("Helge", "Hej!", DateTimeOffset.UtcNow.ToUnixTimeSeconds()
+database.Store(cheep);
+...
+public record Cheep(string Author, string Message, long Timestamp);
+*/
