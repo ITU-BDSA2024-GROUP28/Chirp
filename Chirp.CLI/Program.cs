241bd747b63a0a98bd6b03c72a335b2e33d72281
Emma <ekra@itu.dk>
2024-09-04 14:59:34 +0200

    Added docopt package for terminal interface
    Not working yet

--- Chirp.CLI/Program.cs
+++ Chirp.CLI/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿const string usage = @"Chirp CLI version.
+
+Usage:
+  chirp read <limit>
+  chirp cheep <message>
+  chirp (-h | --help)
+  chirp --version
+
+Options:
+  -h --help     Show this screen.
+  --version     Show version.
+";
+
+var arguments = new Docopt().Apply(usage, args, version: "1.0", exit: true)!;
