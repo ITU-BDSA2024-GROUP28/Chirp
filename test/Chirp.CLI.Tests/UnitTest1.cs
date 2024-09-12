23d6f669d26b198c441bd7559132ff6c1bad51c3
KampJN <josefinekampn@gmail.com>
2024-09-12 15:59:30 +0200

    Added a basic structure for the tests. They are not complete. Co-Authored-by: Kulsum <umab@itu.dk> Co-Authored-by: Amira <aayo@itu.dk> Co-Authored-by: Stine <sths@itu.dk> Co-Authored-by: Emma <ekra@itu.dk>

--- test/Chirp.CLI.Tests/UnitTest1.cs
+++ test/Chirp.CLI.Tests/UnitTest1.cs
@@ -3,8 +3,18 @@ namespace Chirp.CLI.Tests;
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void TimestampTest()
     {
+        //The test is not done, but the base for the test should look somthing like this
+        
+        //int time = 1045;
+        //string actual = "Insers correct answer";
+        
+        //Asses.Equal(time, actual);
+    }
 
+    public void OutputTest()
+    {
+        
     }
 }
--- test/SimpleDB.Tests/UnitTest1.cs
+++ test/SimpleDB.Tests/UnitTest1.cs
@@ -3,8 +3,12 @@ namespace SimpleDB.Tests;
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void GetCheepTest()
     {
-
+        //Cheep cheep = new Cheep("kajn", "Hej!", 0);
+        //Store the cheep
+        //Get the cheep
+        
+        //Assert if the gotten cheep equals the created one
     }
 }
