31e79bacc61ace50f583b2e2aa66033f53053124
Emma <ekra@itu.dk>
2024-09-16 10:27:57 +0200

    Testing release workflow

--- SimpleDB/CSVDatabase.cs
+++ SimpleDB/CSVDatabase.cs
@@ -6,6 +6,7 @@ namespace SimpleDB;
 public sealed class CSVDatabase<T>:IDatabaseRepository<T> {
     public IEnumerable<T> Read(int? limit = null)
     {
+        // Testing the release workflow
 
         IEnumerable<T> information;
         
