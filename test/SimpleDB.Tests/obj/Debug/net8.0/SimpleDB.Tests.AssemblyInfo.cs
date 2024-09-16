cb83ae355f85f7e44f79058b606477ade6ae0f61
sths <stinehelena09@gmail.com>
2024-09-16 10:30:12 +0200

    Removed empty directories Co-Authored-by: Kulsum <umab@itu.dk> Co-Authored-by: Amira <aayo@itu.dk> Co-Authored-by: Josefine <kajn@itu.dk> Co-Authored-by: Emma <ekra@itu.dk>

--- test/SimpleDB.Tests/obj/Debug/net8.0/SimpleDB.Tests.AssemblyInfo.cs
+++ test/SimpleDB.Tests/obj/Debug/net8.0/SimpleDB.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SimpleDB.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23d6f669d26b198c441bd7559132ff6c1bad51c3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dbeeace4f868c0c7c8bf6a35b41afc5cec56b0de")]
 [assembly: System.Reflection.AssemblyProductAttribute("SimpleDB.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SimpleDB.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
