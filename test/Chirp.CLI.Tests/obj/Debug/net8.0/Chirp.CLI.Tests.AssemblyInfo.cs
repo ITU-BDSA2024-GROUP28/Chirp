e398d72f582556c32186e293738614f61d892035
sths <stinehelena09@gmail.com>
2024-09-17 08:43:30 +0200

    Everything should build now Co-Authored-by: Kulsum <umab@itu.dk> Co-Authored-by: Amira <aayo@itu.dk> Co-Authored-by: Josefine <kajn@itu.dk> Co-Authored-by: Emma <ekra@itu.dk>

--- test/Chirp.CLI.Tests/obj/Debug/net8.0/Chirp.CLI.Tests.AssemblyInfo.cs
+++ test/Chirp.CLI.Tests/obj/Debug/net8.0/Chirp.CLI.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Chirp.CLI.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dbeeace4f868c0c7c8bf6a35b41afc5cec56b0de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37ded68ab92c0783ba1d0564ad3a6cc44d57a114")]
 [assembly: System.Reflection.AssemblyProductAttribute("Chirp.CLI.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Chirp.CLI.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
