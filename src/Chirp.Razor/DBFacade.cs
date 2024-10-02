874722c6456bc006ff85ee5f2fc821d411702488
Um-Kulsum Abusheva <umab@itu.dk>
2024-10-02 22:42:21 +0200

    environmentVariableCheck created

--- src/Chirp.Razor/DBFacade.cs
+++ src/Chirp.Razor/DBFacade.cs
@@ -1,11 +1,28 @@
 using Microsoft.Data.Sqlite;
 
-public class DBFacade
+public class DBFacade 
 {
-    string sqlDBFilePath = "../../chirp.db";
-
+    string sqlDBFilePath = "";
+    
     string sqlQuery = @"SELECT * FROM message ORDER by message.pub_date desc";
+    
+    
+    public DBFacade()
+    {
+        // get the environment variable for CHIRPDBPATH
+        string dbPath = Environment.GetEnvironmentVariable("CHIRPDBPATH");
 
+        // if CHIRPDBPATH is not set, use the user's temporary directory and default to chirp.db
+        if (string.IsNullOrEmpty(dbPath))
+        {
+            // combine the user's temp directory with "chirp.db" as the default filename
+            dbPath = Path.Combine(Path.GetTempPath(), "chirp.db");
+        }
+    
+        // assign the final path to sqlDBFilePath for use
+        sqlDBFilePath = dbPath;
+    }
+    
       public void GetCheeps(){
           
           using (var connection = new SqliteConnection($"Data Source={sqlDBFilePath}"))
