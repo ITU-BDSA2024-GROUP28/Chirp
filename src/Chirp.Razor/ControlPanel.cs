e70e55c185a1ccced211b571ddd7d05d18d3f1db
KampJN <josefinekampn@gmail.com>
2024-10-03 20:13:19 +0200

    Refactored the ControlPanel.cs class so there's a function to pagise an array of cheeps. Then determineCheeps() to return a page of cheeps

--- src/Chirp.Razor/ControlPanel.cs
+++ src/Chirp.Razor/ControlPanel.cs
@@ -3,6 +3,7 @@ namespace SimpleDB;
 public class ControlPanel
 {
     public int pageCounter;
+    public int[][] pagedCheeps;
     // Function thet gets input from the buttons to determine what page the user is on
     // Consider if this is necessary when there is a pageCounter
     public static int determinePage()
@@ -10,13 +11,18 @@ public class ControlPanel
         return pageCounter;
     }
 
-    // Function that returns the cheeps based on the current page
     public static int[] determineCheeps(int pageNr)
     {
+        return pagedCheeps[pageNr];
+    }
+    
+    // Function that returns the cheeps based on the current page
+    public static pagiseCheeps(int[] cheeps)
+    {
         // Array of cheepId from the DB, sorted by recently posted
-        int[] allCheeps;
+        int[] cheeps;
         int cheepCounter = 0;
-        int pages = Math.Cieling(allCheeps.Length / 20); // Get's the needed amount of pages
+        int pages = Math.Cieling(cheeps.Length / 20); // Get's the needed amount of pages
         int[pages][20] pagedCheeps;
 
         for (int i = 0; i < pages; i++) // Loop to go through the pages
