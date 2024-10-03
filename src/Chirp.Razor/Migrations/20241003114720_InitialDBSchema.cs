0dd22b27eb186dff4008a78df322984fb227dc5f
Emma <ekra@itu.dk>
2024-10-03 13:48:19 +0200

    Migrated to EF Core
    Chat.db was built

---
+++ src/Chirp.Razor/Migrations/20241003114720_InitialDBSchema.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Chirp.Razor.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialDBSchema : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    AuthorId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Email = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_users", x => x.AuthorId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "cheeps",
+                columns: table => new
+                {
+                    CheepId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    AuthorID = table.Column<int>(type: "INTEGER", nullable: false),
+                    Text = table.Column<string>(type: "TEXT", nullable: false),
+                    TimeStamp = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_cheeps", x => x.CheepId);
+                    table.ForeignKey(
+                        name: "FK_cheeps_users_AuthorID",
+                        column: x => x.AuthorID,
+                        principalTable: "users",
+                        principalColumn: "AuthorId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_cheeps_AuthorID",
+                table: "cheeps",
+                column: "AuthorID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "cheeps");
+
+            migrationBuilder.DropTable(
+                name: "users");
+        }
+    }
+}
