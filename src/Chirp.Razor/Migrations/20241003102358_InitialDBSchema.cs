de2bee03b532967c94b6575bcf5cc1a23e369db0
Emma <ekra@itu.dk>
2024-10-03 12:25:23 +0200

    Created an EF migration
    Co-Authored-by: Kulsum <umab@itu.dk>
    Co-Authored-by: Amira <aayo@itu.dk>
    Co-Authored-by: Stine <sths@itu.dk>
    Co-Authored-by: Josefine <kajn@itu.dk>

---
+++ src/Chirp.Razor/Migrations/20241003102358_InitialDBSchema.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Chirp.Razor.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialDBSchema : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_users", x => x.UserId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "messages",
+                columns: table => new
+                {
+                    MessageId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    UserID = table.Column<int>(type: "INTEGER", nullable: false),
+                    Text = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_messages", x => x.MessageId);
+                    table.ForeignKey(
+                        name: "FK_messages_users_UserID",
+                        column: x => x.UserID,
+                        principalTable: "users",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_messages_UserID",
+                table: "messages",
+                column: "UserID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "messages");
+
+            migrationBuilder.DropTable(
+                name: "users");
+        }
+    }
+}
