001cf35550576b88f01314b996cba4bd4172ab47
Emma <ekra@itu.dk>
2024-10-07 11:16:42 +0200

    Created EFCore project
    Added the domain model
    imported packages
    made structure of cheep and author
    
    Co-Authored-by: Kulsum <umab@itu.dk>
    Co-Authored-by: Amira <aayo@itu.dk>
    Co-Authored-by: Stine <sths@itu.dk>
    Co-Authored-by: Josefine <kajn@itu.dk>

---
+++ src/Chirp.Core/Domain-Model/Author.cs
@@ -0,0 +1,9 @@
+namespace Domain-Model;
+
+public class Author
+{
+    public int AuthorID { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public ICollection<Cheep> Cheeps { get; set; }
+}
---
+++ src/Chirp.Core/Domain-Model/Cheep.cs
@@ -0,0 +1,10 @@
+namespace Domain-Model;
+
+public class Cheep
+{
+    public int CheepID { get; set; }
+    public int AuthorID { get; set; }
+    public string Text { get; set; }
+    public long Timestamp { get; set; }
+    public Author Author { get; set; }
+}
