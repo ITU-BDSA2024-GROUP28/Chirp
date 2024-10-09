9491b7e58fa2c2b8fa945d0a4652bc412a274fa1
Amira Maria Ayoub <aayo@itu.dk>
2024-10-09 19:02:53 +0200

    Cheep limit start

--- src/Chirp.Core/DomainModel/Cheep.cs
+++ src/Chirp.Core/DomainModel/Cheep.cs
@@ -1,9 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace DomainModel;
 
 public class Cheep
 {
     public int CheepID { get; set; }
     public int AuthorID { get; set; }
+    [Required]
+    [StringLength(160)]
     public string Text { get; set; }
     public long Timestamp { get; set; }
     public Author Author { get; set; }
