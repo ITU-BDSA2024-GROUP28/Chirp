4f6115fc152c1e5ee98f8fd398c523c9512e1ad7
KampJN <josefinekampn@gmail.com>
2024-10-30 13:40:28 +0100

    Removed excessive asterisks
    Removed excessive asterisks in the documentation in Chirp.Core

--- src/Chirp.Core/Author.cs
+++ src/Chirp.Core/Author.cs
@@ -4,19 +4,19 @@ public class Author
 {
     public int AuthorId { get; set; }
     //string limit CAN DO
-    /**
+    /*
      * AuthorId retrieves the current Author id, and sets it as the id. 
      */
     public required string Name { get; set; }
-    /**
+    /*
      * Name retrieves the current name, then sets it as the name
      */
     public required string Email { get; set; }
-    /**
+    /*
      * Email retrieves the current email, then sets it as the email
      */
     public ICollection<Cheep>? Cheeps { get; set; }
-    /**
+    /*
      * Cheeps retrieves the relevant cheeps from the author and sets them as the collection
      */
 }
--- src/Chirp.Core/AuthorDTO.cs
+++ src/Chirp.Core/AuthorDTO.cs
@@ -3,15 +3,15 @@ namespace Chirp.Core;
 public class AuthorDTO
 {
     public string Name { get; set; }
-    /**
+    /*
      * Name retrieves the current name, then sets it as the name
      */
     public string Email { get; set; }
-    /**
+    /*
      * Email retrieves the current email, then sets it as the email
      */
     public int Id { get; set; }
-    /**
+    /*
      * Id retrieves the current id and sets it as the id.
      */
     public AuthorDTO(string name, string email, int id)
@@ -20,7 +20,7 @@ public class AuthorDTO
         Email = email;
         Id = id;
     }
-    /**
+    /*
      * AuthorDTO takes the arguments name, email and id
      * These are used to set the arguments as the values of Name, Email and Id
      */
