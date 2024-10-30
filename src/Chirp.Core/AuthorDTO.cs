2949462fde78480d679469ec7ac3d59afb30457c
KampJN <josefinekampn@gmail.com>
2024-10-30 13:24:06 +0100

    Documented AuthorDTO.cs

--- src/Chirp.Core/AuthorDTO.cs
+++ src/Chirp.Core/AuthorDTO.cs
@@ -3,13 +3,25 @@ namespace Chirp.Core;
 public class AuthorDTO
 {
     public string Name { get; set; }
+    /**
+     * Name retrieves the name, then sets it as the name
+     */
     public string Email { get; set; }
+    /**
+     * Email retrieves the email, then sets it as the email
+     */
     public int Id { get; set; }
-
+    /**
+     * Id retrieves the Author id and sets it as the id.
+     */
     public AuthorDTO(string name, string email, int id)
     {
         Name = name;
         Email = email;
         Id = id;
     }
+    /**
+     * AuthorDTO takes the arguments name, email and id
+     * These are used to set the arguments as the values of Name, Email and Id
+     */
 }
