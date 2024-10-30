98fbeeb4a1c0d021bc877d4ccc1c119d4435b213
Emma <ekra@itu.dk>
2024-10-30 15:04:34 +0100

    Unit test 1 working

---
+++ test/Chirp.Infrastructure.Unittests/CheepServiceUnitTest.cs
@@ -0,0 +1,50 @@
+using Chirp.Core;
+using Chirp.Infrastructure.Repositories;
+using Chirp.Infrastructure.Services;
+using Microsoft.AspNetCore.Mvc.Diagnostics;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Moq;
+
+namespace Chirp.Infrastructure.Unittests;
+
+public class CheapServiceUnitTest
+{
+    
+    private ServiceProvider _serviceProvider;
+
+    public CheapServiceUnitTest()
+    {
+        SetUp();
+    }
+
+    public void SetUp()
+    {
+        var services = new ServiceCollection();
+        
+        // use in memory database to test
+        services.AddDbContext<ChirpDbContext>(options => options.UseInMemoryDatabase("InMemoryDatabase"));
+        
+        services.AddScoped<ICheepRepository, CheepRepository>();
+        services.AddScoped<IAuthorRepository, AuthorRepository>();
+        services.AddScoped<ICheepService, CheepService>();
+        
+        _serviceProvider = services.BuildServiceProvider();
+    }
+    
+    [Fact]
+    public void Test1()
+    {
+        // Arrange
+        using var scope = _serviceProvider.CreateScope();
+        {
+            var scopedServices = scope.ServiceProvider;
+            var cheepService = scopedServices.GetRequiredService<ICheepService>();
+            
+            List<CheepDTO> cheeps = new List<CheepDTO>();
+            cheeps = cheepService.GetCheeps(0);
+            Assert.NotEmpty(cheeps);
+        }
+    }
+}
