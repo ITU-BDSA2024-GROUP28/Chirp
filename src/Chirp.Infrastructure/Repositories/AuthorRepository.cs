2e77741ff9dd713a2ddb8a8c8b2bfeb42e302806
KampJN <josefinekampn@gmail.com>
2024-10-30 14:05:57 +0100

    Documented Cheep- and AuthorRepository

--- src/Chirp.Infrastructure/Repositories/AuthorRepository.cs
+++ src/Chirp.Infrastructure/Repositories/AuthorRepository.cs
@@ -8,10 +8,20 @@ public class AuthorRepository : IAuthorRepository
     {
         return new AuthorDTO(author.Name, author.Email, author.AuthorId);
     }
+    /*
+     * Method that creates Author from existing AuthorDTO that EF Core uses to update database.
+     * @param AuthorDTO
+     * @return Author
+     */
 
     public Author ReadAuthor(AuthorDTO authorDTO)
     {
         throw new NotImplementedException();
         //Create instead, set defaults
     }
+    /*
+     * Method that creates AuthorDTO from existing Author that EF Core uses to update database.
+     * @param a Author
+     * @return AuthorDTO
+     */
 }
--- src/Chirp.Infrastructure/Repositories/CheepRepository.cs
+++ src/Chirp.Infrastructure/Repositories/CheepRepository.cs
@@ -5,36 +5,23 @@ namespace Chirp.Infrastructure.Repositories;
 public class CheepRepository : ICheepRepository
 {
     
+    public CheepDTO ReadCheep(Cheep cheep)
+    {
+        return new CheepDTO(cheep.Text, Convert(cheep.TimeStamp), cheep.Author.Name);
+    }
     /*
      * Method that creates Cheep from existing CheepDTO that EF Core uses to update database.
      * @param a CheepDTO
      * @return Cheep object
      */
-    
-
-    public CheepDTO ReadCheep(Cheep cheep)
-    {
-        return new CheepDTO(cheep.Text, Convert(cheep.TimeStamp), cheep.Author.Name);
-        //returns cheep med timestamp i sec siden 1970, skal være DateTime
-        //men Cheep.cs må kun bruge simple types
-    }
-
-   
-    
-    /*public Cheep ReadCheep(CheepDTO cheepDTO)
-    {
-        Cheep cheep = new Cheep();
-        return cheep;
-    }
-    
-    /*public Author ReadAuthor(AuthorDTO authorDTO)
-    {
-        Author author = new Author();
-        return author;
-    }*/ 
 
     public static long Convert(DateTime dateTime)
     {
         return((DateTimeOffset)dateTime).ToUnixTimeSeconds();
     }
+    /*
+     * Method to convert the time and date to UnixTime
+     * @param Datetime
+     * @return a long
+     */
 }
