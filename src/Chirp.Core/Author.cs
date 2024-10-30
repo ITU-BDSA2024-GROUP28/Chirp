4856d23ced41efd1c19029d3b3a0328a6d2b601e
KampJN <josefinekampn@gmail.com>
2024-10-30 13:32:09 +0100

    Corrected documentation in Author.cs

--- src/Chirp.Core/Author.cs
+++ src/Chirp.Core/Author.cs
@@ -5,18 +5,18 @@ public class Author
     public int AuthorId { get; set; }
     //string limit CAN DO
     /**
-     * AuthorId retrieves the Author id and sets it as the id. 
+     * AuthorId retrieves the current Author id, and sets it as the id. 
      */
     public required string Name { get; set; }
     /**
-     * Name retrieves the name, then sets it as the name
+     * Name retrieves the current name, then sets it as the name
      */
     public required string Email { get; set; }
     /**
-     * Email retrieves the email, then sets it as the email
+     * Email retrieves the current email, then sets it as the email
      */
     public ICollection<Cheep>? Cheeps { get; set; }
     /**
-     * Cheeps retrieves the cheeps from the author and sets them as the collection
+     * Cheeps retrieves the relevant cheeps from the author and sets them as the collection
      */
 }
