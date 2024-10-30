0fbefca7783550efd56f4a0fd4fe06ab9ab7148e
KampJN <josefinekampn@gmail.com>
2024-10-30 13:30:58 +0100

    Documented Cheep.cs

--- src/Chirp.Core/Cheep.cs
+++ src/Chirp.Core/Cheep.cs
@@ -3,8 +3,23 @@ namespace Chirp.Core;
 public class Cheep
 {
     public int CheepId { get; set; }
+    /**
+     * CheepId retrieves the id of the current cheep, and sets it as the value
+     */
     public int AuthorId { get; set; }
+    /**
+     * AuthorId retrieves the id of the current author, and sets it as the value
+     */
     public required string Text { get; set; }
+    /**
+     * Text retrieves the text of the current cheep, and sets it as the current value
+     */
     public DateTime TimeStamp { get; set; }
+    /**
+     * TimeStamp retrieves the timestamp of the current cheep, and sets it as the value
+     */
     public required Author Author { get; set; }
+    /**
+     * Author retrieves the author of the current cheep, ans sets it as the value
+     */
 }
