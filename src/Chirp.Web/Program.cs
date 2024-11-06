1d0a6eda4bdb4b590cbaaff0ae01262f1470bbbe
Amira Maria Ayoub <aayo@itu.dk>
2024-11-06 15:42:22 +0100

    changed nav bar
    Co-Authored-By: Um-Kulsum Abusheva <151044780+Kulsumitu@users.noreply.github.com>

--- src/Chirp.Web/Program.cs
+++ src/Chirp.Web/Program.cs
@@ -27,8 +27,8 @@ builder.Services.AddAuthentication(options =>
     //.AddCookie()
     .AddGitHub(o =>
     {
-        o.ClientId = builder.Configuration["authentication:github:clientId"];
-        o.ClientSecret = builder.Configuration["authentication:github:clientSecret"];
+        o.ClientId = builder.Configuration["authentication:github:clientId"]?? throw new ArgumentNullException();
+        o.ClientSecret = builder.Configuration["authentication:github:clientSecret"]?? throw new ArgumentNullException();
         o.CallbackPath = "/signin-github";
     });
 
