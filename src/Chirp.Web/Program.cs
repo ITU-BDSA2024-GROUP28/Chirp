707ea6826c7a9ba71991d6df79198b96505a3161
Emma <ekra@itu.dk>
2024-11-08 14:46:31 +0100

    Added environment variable

--- src/Chirp.Web/Program.cs
+++ src/Chirp.Web/Program.cs
@@ -29,7 +29,7 @@ builder.Services.AddAuthentication(options =>
     .AddGitHub(o =>
     {
         o.ClientId = builder.Configuration["authentication:github:clientId"]
-                     ?? Environment.GetEnvironmentVariable("GITHUB_PROVIDER_AUTHENTICATION_ID")
+                     ?? Environment.GetEnvironmentVariable("GITHUB_PROVIDER_AUTHENTICATION_ID") // Gotten path from Azure
                      ?? throw new InvalidOperationException("You must provide an authentication client ID.");
         o.ClientSecret = builder.Configuration["authentication:github:clientSecret"] 
                          ?? Environment.GetEnvironmentVariable("GITHUB_PROVIDER_AUTHENTICATION_SECRET")
