1c3c97bcd64c86b1a9a688cfa95bda0e562e51ad
Emma <ekra@itu.dk>
2024-11-08 15:47:46 +0100

    Added local oAuth

--- src/Chirp.Web/Program.cs
+++ src/Chirp.Web/Program.cs
@@ -29,9 +29,11 @@ builder.Services.AddAuthentication(options =>
     .AddCookie()
     .AddGitHub(o =>
     {
-        o.ClientId = Environment.GetEnvironmentVariable("GITHUB_PROVIDER_AUTHENTICATION_ID") // Gotten path from Azure
+        o.ClientId = builder.Configuration["authentication:github:clientId"] 
+                     ?? Environment.GetEnvironmentVariable("GITHUB_PROVIDER_AUTHENTICATION_ID") // Gotten path from Azure
                      ?? throw new InvalidOperationException("You must provide an authentication client ID.");
-        o.ClientSecret = Environment.GetEnvironmentVariable("GITHUB_PROVIDER_AUTHENTICATION_SECRET")
+        o.ClientSecret = builder.Configuration["authentication:github:clientSecret"] 
+                         ?? Environment.GetEnvironmentVariable("GITHUB_PROVIDER_AUTHENTICATION_SECRET")
                          ?? throw new InvalidOperationException("You must provide an authentication client Secret.");
         o.CallbackPath = "/signin-github";
     });
