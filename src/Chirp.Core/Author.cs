4d42f7012e52226f3764b9f95f3944728970ad36
Emma <ekra@itu.dk>
2024-11-20 15:27:46 +0100

    Author is identtity user Co-Authored-by: Kulsum <umab@itu.dk> Co-Authored-by: Amira <aayo@itu.dk> Co-Authored-by: Stine <sths@itu.dk> Co-Authored-by: Josefine <kajn@itu.dk>

--- src/Chirp.Core/Author.cs
+++ src/Chirp.Core/Author.cs
@@ -1,21 +1,10 @@
+using Microsoft.AspNetCore.Identity;
+
 namespace Chirp.Core;
 
-public class Author
+public class Author : IdentityUser<int>
 {
-    public int AuthorId { get; set; }
-    //string limit CAN DO
-    /*
-     * AuthorId retrieves the current Author id, and sets it as the id. 
-     */
-    public required string Name { get; set; }
-    /*
-     * Name retrieves the current name, then sets it as the name
-     */
-    public required string Email { get; set; }
-    /*
-     * Email retrieves the current email, then sets it as the email
-     */
-    public ICollection<Cheep>? Cheeps { get; set; }
+    public required ICollection<Cheep> Cheeps { get; set; }
     /*
      * Cheeps retrieves the relevant cheeps from the author and sets them as the collection
      */
