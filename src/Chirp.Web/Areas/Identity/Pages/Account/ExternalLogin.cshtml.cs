3c988d4ffb1aa54c9bda02e7c2e2d1bbf5667121
Amira Maria Ayoub <aayo@itu.dk>
2024-11-26 12:53:08 +0100

    redirects from github login to timeline

--- src/Chirp.Web/Areas/Identity/Pages/Account/ExternalLogin.cshtml.cs
+++ src/Chirp.Web/Areas/Identity/Pages/Account/ExternalLogin.cshtml.cs
@@ -114,6 +114,7 @@ namespace Chirp.Web.Areas.Identity.Pages.Account
 
             // Sign in the user with this external login provider if the user already has a login.
             var result = await _signInManager.ExternalLoginSignInAsync(info.LoginProvider, info.ProviderKey, isPersistent: false, bypassTwoFactor: true);
+            
             if (result.Succeeded)
             {
                 _logger.LogInformation("{Name} logged in with {LoginProvider} provider.", info.Principal.Identity.Name, info.LoginProvider);
@@ -135,6 +136,7 @@ namespace Chirp.Web.Areas.Identity.Pages.Account
                         Username = info.Principal.FindFirstValue(ClaimTypes.Name)
                     };
                 }
+                //ClaimTypes.Email
                 return Page();
             }
         }
@@ -167,6 +169,7 @@ namespace Chirp.Web.Areas.Identity.Pages.Account
 
                         var userId = await _userManager.GetUserIdAsync(user);
                         var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                        //user.EmailConfirmed=true
                         code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
                         var callbackUrl = Url.Page(
                             "/Account/ConfirmEmail",
@@ -174,14 +177,14 @@ namespace Chirp.Web.Areas.Identity.Pages.Account
                             values: new { area = "Identity", userId = userId, code = code },
                             protocol: Request.Scheme);
 
-                       /* await _emailSender.SendEmailAsync(info.Principal.Identity.Name!, "Confirm your email",
+                       await _emailSender.SendEmailAsync(info.Principal.Identity.Name!, "Confirm your email",
                             $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
 
                         // If account confirmation is required, we need to show the link if we don't have a real email sender
                         if (_userManager.Options.SignIn.RequireConfirmedAccount)
                         {
                             return RedirectToPage("./RegisterConfirmation", new { Email = info.Principal.Identity.Name });
-                        }*/
+                        }
 
                         await _signInManager.SignInAsync(user, isPersistent: false, info.LoginProvider);
                         return LocalRedirect(returnUrl);
