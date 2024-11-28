bb061e4d10eaa7b367b25e789572d40a7f5a7b9e
Emma <ekra@itu.dk>
2024-11-28 15:02:31 +0100

    Possible to register

--- src/Chirp.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ src/Chirp.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -120,14 +120,10 @@ namespace Chirp.Web.Areas.Identity.Pages.Account
             if (ModelState.IsValid)
             {
                 var user = CreateUser();
-                user.UserName = Input.Username;
-                user.Email = Input.Email;
                 await _userStore.SetUserNameAsync(user, Input.Username, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
                 var result = await _userManager.CreateAsync(user, Input.Password);
-                _context.Authors.Add(user);
-                _context.SaveChanges();
-
+                
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User created a new account with password.");
