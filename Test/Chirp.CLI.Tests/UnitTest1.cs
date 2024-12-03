0ccf95499b84499bc472150a8b283244571cffc2
Emma <ekra@itu.dk>
2024-12-03 11:08:26 +0100

    Test one implemented

--- Test/Chirp.CLI.Tests/UnitTest1.cs
+++ Test/Chirp.CLI.Tests/UnitTest1.cs
@@ -1,9 +1,24 @@
+using Chirp.CLI;
+using SimpleDB;
 namespace Chirp.CLI.Tests;
 
 public class UnitTest1
 {
+    CsvDatabase<Cheep> csvDatabase;
+
+    public UnitTest1()
+    {
+        csvDatabase = CsvDatabase<Cheep>.GetInstance();
+    }
     [Fact]
     public void Test1()
     {
     }
+    
+    [Fact]  
+    public void CSVDatabase_is_singleton()  
+    {  
+        var dbInstanceTwo = CsvDatabase<Cheep>.GetInstance();  
+        Assert.Equal(dbInstanceTwo, csvDatabase);    //uses method AreEqual to check if they are the same  
+    }  
 }
