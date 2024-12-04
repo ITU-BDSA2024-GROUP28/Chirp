befdc8ce6183dcabe56e692039b2adc8d292c1f8
KampJN <josefinekampn@gmail.com>
2024-12-04 22:24:59 +0100

    User can get lists of their following/followers

---
+++ src/Chirp.Web/Pages/Followers.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Chirp.Core;
+using Chirp.Infrastructure.Services;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Chirp.Web.Pages;
+
+public class Followers : PageModel
+{
+    private readonly IFollowService _followService;
+    public List<AuthorDTO> ListOfFollowers;
+    
+    public Followers(IFollowService followService)
+    {
+        _followService = followService;
+    }
+    public void OnGet()
+    {
+        ListOfFollowers = _followService.GetFollowers(User.Identity.Name);
+    }
+}
