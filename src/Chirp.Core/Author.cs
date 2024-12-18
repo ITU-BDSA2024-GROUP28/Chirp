8641a6657a7a76508af5e98a9a496f3715b4be2e
KampJN <josefinekampn@gmail.com>
2024-12-18 14:49:14 +0100

    Corrected documentation in Core

--- src/Chirp.Core/Author.cs
+++ src/Chirp.Core/Author.cs
@@ -5,9 +5,6 @@ namespace Chirp.Core;
 public class Author : IdentityUser<int>
 {
     public required ICollection<Cheep> Cheeps { get; set; }
-    /*
-     * Cheeps retrieves the relevant cheeps from the author and sets them as the collection
-     */
 
     public List<Author> Following { get; set; } = [];
 
